012bd313be9036a0e568d01150515e4c0a8fcf80
Thinh Duong <tanthinhdt24@gmail.com>
2024-05-15 11:03:32 +0700

    feat(src/models): add src code for model

---
+++ src/models/eval.py
@@ -0,0 +1,31 @@
+import os
+import hydra
+import torch
+import logging
+from pytorch_lightning import Trainer
+from src.features.data_module import DataModule
+
+
+@hydra.main(version_base="1.3", config_path="configs", config_name="config")
+def main(cfg):
+    # Set modules and trainer
+    if cfg.data.modality in ["audio", "visual"]:
+        from lightning import ModelModule
+    elif cfg.data.modality == "audiovisual":
+        from lightning_av import ModelModule
+    modelmodule = ModelModule(cfg)
+    datamodule = DataModule(cfg)
+    trainer = Trainer(num_nodes=1, gpus=1)
+    # Training and testing
+    modelmodule.model.load_state_dict(
+        torch.load(
+            cfg.pretrained_model_path,
+            map_location=lambda storage,
+            loc: storage
+        )
+    )
+    trainer.test(model=modelmodule, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    main()
